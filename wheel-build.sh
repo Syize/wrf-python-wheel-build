76c7fbe3b7bc25d5eb0f33b7d96e1eeea44a6eb3
Syize <syizeliu@gmail.com>
2026-02-06 16:36:51 +0800

    Update pyproject to avoid build error.

--- wheel-build.sh
+++ wheel-build.sh
@@ -2,14 +2,14 @@
 
 for num in $(seq 10 14);
 do
-    if [ -e .venv ]
-    then
-        rm -rf .venv
-    fi
+    # if [ -e .venv ]
+    # then
+    #     rm -rf .venv
+    # fi
 
-    uv venv --python "3.${num}"
+    uv venv --python "3.${num}" --clear
     source .venv/bin/activate
-    uv pip install build
+    uv pip install build charset-normalizer
     python_path=`which python`
     echo "########## Use python from: ${python_path} ###########"
     python -m build
